90c2197a45aca2b3597f95a048c1f4175e4680f3
ShackyShell <shackyshell@gmail.com>
2025-12-15 14:17:53 +0100

    routing po odswiezeniu strony - fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path";
+import { copyFileSync } from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,12 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    {
+      name: 'copy-404',
+      closeBundle() {
+        copyFileSync('dist/index.html', 'dist/404.html');
+      }
+    }
   ],
    resolve: {
      alias: {
